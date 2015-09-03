a9d54e770bdef38b94f1aa058e42f37d42fa90cf
hiradate <m.hiradate@gmail.com>
2015-09-03 12:41:37 +0000

    debug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,7 +11,11 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = false
+  # config.consider_all_requests_local = true にすると
+  # heroku でもエラーの詳細が表示されるようになります
+  # 通常時は　false　にしておくこと！
+  #config.consider_all_requests_local       = false
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
